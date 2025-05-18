3ef7af5aeb29c49226d8c1426f348ee4bd9c4998
Yahyaelshazly <50187004+Yahyaelshazly@users.noreply.github.com>
2025-05-18 19:23:51 +0300

    online_retail_table.sql

---
+++ online_retail_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE OnlineRetail (
+    InvoiceNo VARCHAR(20),
+    StockCode VARCHAR(20),
+    Description NVARCHAR(255),
+    Quantity INT,
+    InvoiceDate DATETIME,
+    UnitPrice FLOAT,
+    CustomerID VARCHAR(20),
+    Country VARCHAR(50),
+    TotalPrice FLOAT,
+    Year INT,
+    Month INT
+);
